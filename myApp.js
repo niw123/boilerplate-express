477ba8772f7dc95c1e02cc2d081c98337cbb09a6
Naruto Iwasaki <niw206@gmail.com>
2018-10-22 21:41:01 -0700

    serve /views/index.html

--- myApp.js
+++ myApp.js
@@ -13,11 +13,16 @@ console.log("Hello World")
 
 
 /** 2) A first working Express Server */
+/*
 app.get('/', (req, res) => {
   res.send('Hello Express');
 })
+*/
 
 /** 3) Serve an HTML file */
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 
 /** 4) Serve static assets  */
