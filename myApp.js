457e7fbc70dd2876e9cc5c1cfa5ca7f27e7a6021
Naruto Iwasaki <niw206@gmail.com>
2018-10-24 09:12:07 -0700

    Capitalize response json

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config()
+
 // --> 7)  Mount the Logger middleware here
 
 
@@ -30,11 +32,20 @@ app.use(express.static(__dirname + '/public'))
 
 
 /** 5) serve JSON on a specific route */
+/*
 app.get('/json', (req, res) => {
   res.json({"message": "Hello json"})
 })
+*/
 
 /** 6) Use the .env file to configure the app */
+app.get('/json', (req, res) => {
+  let message = "Hello json"
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase()
+  }
+  res.json({"message": message })
+})
  
  
 /** 7) Root-level Middleware - A logger */
